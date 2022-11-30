54e6e7945ecf5d591136c2d97d5f8ce54d695fae
bottleofench <71398923+bottleofench@users.noreply.github.com>
2022-11-30 23:59:31 +0600

    just test

--- src/main/java/api/bottleofench/httplibrary/HttpLibrary.java
+++ src/main/java/api/bottleofench/httplibrary/HttpLibrary.java
@@ -10,23 +10,41 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class HttpLibrary {
-    public static String get(String urlWithParams, String token) throws IOException {
+    public static String get(String urlWithParams, String token) {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpGet get = new HttpGet(urlWithParams);
         get.setHeader("Authorization", token);
-        HttpResponse httpResponse = httpclient.execute(get);
+        HttpResponse httpResponse = null;
+        try {
+            httpResponse = httpclient.execute(get);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         StringBuilder builder = new StringBuilder();
-        new Scanner(httpResponse.getEntity().getContent()).forEachRemaining(builder::append);
+        try {
+            new Scanner(httpResponse.getEntity().getContent()).forEachRemaining(builder::append);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return builder.toString();
     }
 
-    public static String post(String urlWithParams, String token) throws IOException {
+    public static String post(String urlWithParams, String token) {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpPost post = new HttpPost(urlWithParams);
         post.setHeader("Authorization", token);
-        HttpResponse httpResponse = httpclient.execute(post);
+        HttpResponse httpResponse = null;
+        try {
+            httpResponse = httpclient.execute(post);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         StringBuilder builder = new StringBuilder();
-        new Scanner(httpResponse.getEntity().getContent()).forEachRemaining(builder::append);
+        try {
+            new Scanner(httpResponse.getEntity().getContent()).forEachRemaining(builder::append);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return builder.toString();
     }
 }
