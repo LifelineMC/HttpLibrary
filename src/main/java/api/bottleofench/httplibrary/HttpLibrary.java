b7dceba42c0a35f934024ecb992ef6f695fb162c
bottleofench <71398923+bottleofench@users.noreply.github.com>
2022-12-06 19:33:23 +0600

    just test x2

--- src/main/java/api/bottleofench/httplibrary/HttpLibrary.java
+++ src/main/java/api/bottleofench/httplibrary/HttpLibrary.java
@@ -3,48 +3,26 @@ package api.bottleofench.httplibrary;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class HttpLibrary {
-    public static String get(String urlWithParams, String token) {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+    public static String get(String urlWithParams, String token) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet get = new HttpGet(urlWithParams);
         get.setHeader("Authorization", token);
-        HttpResponse httpResponse = null;
-        try {
-            httpResponse = httpclient.execute(get);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        StringBuilder builder = new StringBuilder();
-        try {
-            new Scanner(httpResponse.getEntity().getContent()).forEachRemaining(builder::append);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return builder.toString();
+        HttpResponse response = httpClient.execute(get);
+        return new BasicResponseHandler().handleResponse(response);
     }
 
-    public static String post(String urlWithParams, String token) {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost post = new HttpPost(urlWithParams);
-        post.setHeader("Authorization", token);
-        HttpResponse httpResponse = null;
-        try {
-            httpResponse = httpclient.execute(post);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        StringBuilder builder = new StringBuilder();
-        try {
-            new Scanner(httpResponse.getEntity().getContent()).forEachRemaining(builder::append);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return builder.toString();
+    public static String post(String urlWithParams, String token) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost get = new HttpPost(urlWithParams);
+        get.setHeader("Authorization", token);
+        HttpResponse response = httpClient.execute(get);
+        return new BasicResponseHandler().handleResponse(response);
     }
 }
